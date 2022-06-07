3260b13e68583bf0ea1e5145d70a817125d59190
无敌暴龙战士 <75242799+lwl-650@users.noreply.github.com>
2022-06-07 17:31:30 +0800

    fix: 重新初始化项目

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,24 +7,24 @@ const count = ref(0);
 </script>
 
 <template>
+  <a-steps :current="1">
+    <a-step>
+      <!-- <span slot="title">Finished</span> -->
+      <template #title>Finished</template>
+      <template #description>
+        <span>This is a description.</span>
+      </template>
+    </a-step>
+    <a-step
+      title="In Progress"
+      sub-title="Left 00:00:08"
+      description="This is a description."
+    />
+    <a-step title="Waiting" description="This is a description." />
+  </a-steps>
+
   <h1>{{ msg }}</h1>
-  <div>
-    <a-steps :current="1">
-      <a-step>
-        <!-- <span slot="title">Finished</span> -->
-        <template #title>Finished</template>
-        <template #description>
-          <span>This is a description.</span>
-        </template>
-      </a-step>
-      <a-step
-        title="In Progress"
-        sub-title="Left 00:00:08"
-        description="This is a description."
-      />
-      <a-step title="Waiting" description="This is a description." />
-    </a-steps>
-  </div>
+
   <p>
     Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
